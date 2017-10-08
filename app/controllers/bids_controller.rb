f4695d875bdb0bb35317e1c2a7c6122d542e67d8
Ryvan Firdy <ryvanfirdy@gmail.com>
2017-10-08 17:27:35 +0800

    add view for product

--- app/controllers/bids_controller.rb
+++ app/controllers/bids_controller.rb
@@ -1,2 +1,5 @@
 class BidsController < ApplicationController
+def index
+		@bids = Bid.all
+end
 end
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,25 @@
 class ProductsController < ApplicationController
+		def index
+				@products = Product.all
+		end
+
+		def show
+				@book = Product.find(params[:id])
+		end
+
+		def new
+				@product = Product.new
+		end
+
+		def create
+				product = Product.new(product_params)
+				if product.save
+						redirect_to products_path
+				else
+						render :new
+				end
+		end
+		private def product_params
+				params.require(:product).permit(:name, :starting_price)
+		end
 end
